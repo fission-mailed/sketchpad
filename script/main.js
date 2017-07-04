c69d4018aa9c95af7f9f0c83c1fac8969dd8f778
Michael Long <mikeylong04@googlemail.com>
2017-07-04 02:31:48 +0100

    16x16 grid working

--- script/main.js
+++ script/main.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
+	var x = 16;
+	var y = 16;
 	
-	
-	for(i = 1; i < 10; i++){
-		var div = $('<div class="grid"></div>');
-		div.appendTo('.container');
-		
+	for(i = 1; i <= x; i++){
+		for(j = 1; j <= y; j++){
+			var div = $('<div class="grid"></div>');
+			div.appendTo('.container');
+		};	
 	};
+	
+	var pageWidth = 640 - x;
+	var pageHeight = 640 - y;
+	
+	var boxWidth = Math.round(pageWidth / x) - 1;
+	var boxHeight = Math.round(pageHeight / y) - 1;
+	console.log(boxWidth);
+	
+	$('.grid').css("width", boxWidth);
+	$('.grid').css('height', boxHeight);
+	
 	$('.grid').on('mouseenter', function() {
 		$(this).addClass('entered');
 	});
